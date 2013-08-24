c7d87b6b4cea39f6259aa876c3d44b9f7db55f7a
Chris Cargile lenovo <chris@Vaporware.(none)>
2013-08-24 12:23:26 -0400

    H dded in more

--- gen/com/example/testhack/R.java
+++ gen/com/example/testhack/R.java
@@ -41,8 +41,8 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_secondary=0x7f030001;
-        public static final int borders=0x7f030002;
-        public static final int screen2=0x7f030003;
+        public static final int auth_form=0x7f030002;
+        public static final int mail=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
